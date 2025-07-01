640cba1442c43d68524a1f1baf838a3aef9a2bc7
Makepeace123 <makepeacebanelez@gmail.com>
2025-07-01 04:51:50 +0200

    Update app_1.py

--- app_1.py
+++ app_1.py
@@ -403,7 +403,7 @@ def main():
     
     # Disease Diagnosis Mode
     if app_mode == "Disease Diagnosis":
-        st.title("🍅🌿Tomato Disease Diagnosis🔬🩺")
+        st.title("🍅Tomato Disease Diagnosis🔍")
         st.markdown("Upload a clear photo of a tomato leaf for instant analysis")
         
         # Display history
